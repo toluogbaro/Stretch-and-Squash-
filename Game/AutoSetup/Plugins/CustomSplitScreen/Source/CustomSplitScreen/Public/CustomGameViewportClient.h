0f95ca64e0b53eaa1e6bad8fc5cd7ca83478808b
TAMSIN PAGE <TP247408@falmouth.ac.uk>
2024-04-22 12:06:19 +0100

    Lighting setup changes

---
+++ Game/AutoSetup/Plugins/CustomSplitScreen/Source/CustomSplitScreen/Public/CustomGameViewportClient.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameViewportClient.h"
+#include "Engine/ViewportSplitScreen.h"
+
+#include "CustomGameViewportClient.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class CUSTOMSPLITSCREEN_API UCustomGameViewportClient : public UGameViewportClient
+{
+	GENERATED_BODY()
+
+	UCustomGameViewportClient();
+
+	virtual void UpdateActiveSplitscreenType() override;
+
+	virtual void LayoutPlayers() override;
+};
+
+UENUM(BlueprintType)
+namespace ECustomSplitScreenType
+{
+	enum Type
+	{
+		// Required by UE
+		None,
+		// Custom splitscreen type added after last item in existing enum
+		TwoPlayer = ESplitScreenType::SplitTypeCount,
+		SplitTypeCount
+	};
+}
+
